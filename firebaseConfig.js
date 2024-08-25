5b3e8d9d2a3cd5b1c800822ba4565bfecf65d41a
Daddy4AI <alanqwyuan@gmail.com>
2024-08-25 22:25:48 +0800

    firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+// firebaseConfig.js
+const firebaseConfig = {
+            apiKey: "AIzaSyAoyZelymQJvY13J6SVph46E0UcajCRubw",
+            authDomain: "chineselists.firebaseapp.com",
+            databaseURL: "https://chineselists-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "chineselists",
+            storageBucket: "chineselists.appspot.com",
+            messagingSenderId: "149825999355",
+            appId: "1:149825999355:web:f1478a5e019df1b21bbb5e",
+            measurementId: "G-V7B3T8GG34"
+};
+
+export default firebaseConfig;
+
+
